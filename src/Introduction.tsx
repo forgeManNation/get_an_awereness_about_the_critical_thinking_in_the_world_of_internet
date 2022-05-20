efed1c6dcd9166ee2aac95ea6d159bf54aaf106b
Radek <radek.stary@konicaminolta.com>
2022-05-20 11:04:27 +0200

    progress

---
+++ src/Introduction.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./introduction.css"
+
+const Introduction = () => {
+  return (
+
+    <div className = "introduction" >
+      <h1>Kritické myšlení ve světě internetu</h1>
+    </div>
+  )
+}
+
+export default Introduction
